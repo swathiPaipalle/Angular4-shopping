c6c3b980b5d5db23081565640c1bd3385bd92212
swathi.paipalle <swathi.paipalle@gmail.com>
2017-10-06 16:10:47 -0700

    Initial Commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by spaipalle on 10/6/17.
+ */
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+    templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+
+}

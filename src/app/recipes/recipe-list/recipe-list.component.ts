df2e5cee0fdddcbed5397feaa69c1b9715c6f744
Swathi Paipalle <swathi.paipalle@gmail.com>
2017-10-09 21:38:46 +0000

    update recipe-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
-  constructor() { }
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'this is simply test', 'http://xantilicious.com/wp-content/uploads/2016/11/IMG_0202-copy-1.jpg')
+    ];
+  
+  constructor() {
+    
+    
+  }
 
   ngOnInit() {
   }
